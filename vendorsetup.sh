0352f6f1e0487f5327f78f82569b1acf7250753d
ZenkaBestia <z.mail.6600@gmail.com>
2023-08-04 05:44:58 +0800

    lmi: vendorsetup: track MIUI camera repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,6 @@ git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.g
 echo 'Clone Prebuilt Apps Repo'
 # Prebuilt Apps
 git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-apps -b main device/xiaomi/lmi-prebuilt-apps
+
+echo 'Clone MIUI Camera Repo'
+git clone --depth=1 https://github.com/ppooopppooo/vendor_xiaomi_lmi-miuicamera -b 13.0 vendor/xiaomi/lmi-miuicamera
