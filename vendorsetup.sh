e99152e234aec8741b994fee1cbae30b7101f0b0
ZenkaBestia <z.mail.6600@gmail.com>
2023-10-10 21:20:39 +0800

    lmi: use AOSP Clang 16.0.2 as compiler

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,3 +12,9 @@ git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-ap
 
 echo 'Clone MIUI Camera Repo'
 git clone --depth=1 https://github.com/ppooopppooo/vendor_xiaomi_lmi-miuicamera -b 13.0 vendor/xiaomi/lmi-miuicamera
+
+echo "Download AOSP Clang 16.0.2"
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r475365b.tar.gz
+mkdir prebuilts/clang/host/linux-x86/clang-r475365b
+tar -xvf clang-r475365b.tar.gz -C prebuilts/clang/host/linux-x86/clang-r475365b
+rm clang-r475365b.tar.gz
